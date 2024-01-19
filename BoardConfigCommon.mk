ab7dde1e5f53d34a6a71e30754e831530c524eec
Franklin Albuquerque <126638424+franklin-albuquerque@users.noreply.github.com>
2024-01-19 03:33:36 -0300

    universal990: update blobs from SIHWL1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -157,7 +157,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 BOARD_SECCOMP_POLICY += $(COMMON_PATH)/seccomp
 
 # Security
-VENDOR_SECURITY_PATCH := 2023-12-01
+VENDOR_SECURITY_PATCH := 2024-01-01
 
 # SELinux
 include device/custom/sepolicy/exynos/sepolicy.mk
